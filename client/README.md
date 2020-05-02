0801f5c56d032be098e05a02621be974cfa44545
Ray Hu <ray@isonohealth.com>
2020-05-02 14:06:19 -0700

    update readme and fix electron loading window

--- client/README.md
+++ client/README.md
@@ -1,8 +1,27 @@
 # Client App
 
-npm run pkg to build a command line only tool using pkg
+## run a watcher
+
+npm run watcher
+
+It will listen to localhost:1110
+
+## run the client in command line
+
+npm start localhost:1110
+
+This will launch a command line client connect to the local watcher.
+
+npm start will launch a GUI client, where you can try out.
+
+## Build clients
+
+### Build commandline using pkg
+
+npm run pkg to build a command line only tool using pkg. It is smaller than electron.
 
 run \$0 -h for usage.
 
-npm run pack to build a dual interface tool (command line and GUI) using
-electron
+### Build using electron
+
+npm run pack to build a dual interface tool (command line and GUI) using electron
--- client/main.js
+++ client/main.js
@@ -2,23 +2,54 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
-const client = require('./src/client')
-
 function createWindow() {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({})
+    // console.log(process.argv)
+    // Default arguments array is below. Anything more than that are treated as command line.
+    // [
+    //     'electron.exe',
+    //     '.'
+    // ]
+    let isCalledViaCLI = process.argv && process.argv.length > 2
+    isCalledViaCLI
+        ? console.log('Starting command line')
+        : console.log('Starting GUI')
+    let mainWindow
+    // Create the main browser window
+    if (isCalledViaCLI) {
+        require('./src/client').run()
+        mainWindow = new BrowserWindow({ show: false, width: 0, height: 0 })
+    } else {
+        mainWindow = new BrowserWindow({
+            show: true,
+            width: 1050,
+            height: 600,
+            webPreferences: {
+                nodeIntegration: true,
+                preload: path.join(__dirname, 'preload.js'),
+            },
+        })
 
-    // and load the index.html of the app.
-    mainWindow.loadFile('index.html')
+        // and load the index.html of the app.
+        mainWindow.loadFile('index.html')
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+        // Open the DevTools.
+        mainWindow.webContents.openDevTools()
+    }
+
+    mainWindow.once('ready-to-show', () => {
+        if (isCalledViaCLI) {
+            mainWindow.hide()
+        } else {
+            mainWindow.show()
+            mainWindow.maximize()
+        }
+    })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-//app.whenReady().then(createWindow)
+app.whenReady().then(createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -33,58 +64,5 @@ app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
-app.on('ready', () => {
-    console.log(process.argv)
-    // Default arguments array is below. Anything more than that are treated as command line.
-    // [
-    //     'electron.exe',
-    //     '.'
-    // ]
-    let isCalledViaCLI = process.argv && process.argv.length > 2
-    isCalledViaCLI
-        ? console.log('Starting command line')
-        : console.log('Starting GUI')
-    let mainWindow
-    if (isCalledViaCLI) {
-        client.run()
-        mainWindow = new BrowserWindow({show: false, width: 0, height: 0})
-    } else {
-        mainWindow = new BrowserWindow({
-            show: true,
-            width: 1050,
-            height: 600,
-            webPreferences: {
-                preload: path.join(__dirname, 'preload.js'),
-            },
-        })
-    }
-
-    mainWindow.once('ready-to-show', () => {
-        if (isCalledViaCLI) {
-            mainWindow.hide()
-        } else {
-            mainWindow.show()
-            mainWindow.maximize()
-        }
-    })
-})
-
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-// const {app, BrowserWindow} = require('electron')
-
-// //const client = require('./src/client')
-
-// function createWindow() {
-//     let win = new BrowserWindow({
-//         width: 800,
-//         height: 600,
-//         webPreferences: {
-//             nodeIntegration: true,
-//         },
-//     })
-
-//     win.loadFile('index.html')
-// }
-//app.whenReady().then(createWindow)
