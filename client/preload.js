f84a66531ed462c1b1095da34e778d66064f7fc3
Ray Hu <ray@isonohealth.com>
2020-05-09 09:55:53 -0700

    add prettier

--- client/preload.js
+++ client/preload.js
@@ -1,23 +1,2 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-// eslint-disable-next-line no-undef
-window.addEventListener('DOMContentLoaded', () => {
-
-    // node version
-    const replaceText = (selector, text) => {
-        // eslint-disable-next-line no-undef
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
-
-    for (const type of ['chrome', 'node', 'electron']) {
-        replaceText(`${type}-version`, process.versions[type])
-    }
-
-    // add onclick event for connect button
-    document.getElementById("btnConnect").addEventListener("click", connect);
-
-    document.getElementById("btnWatcher").addEventListener("click", localwatcher);
-    
-
-})
